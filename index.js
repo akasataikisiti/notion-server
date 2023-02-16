5e2ae47439adde253665b6e9d54bfd731311b1a5
akasataikisiti <nama0you0chu@gmail.com>
2023-02-16 19:26:22 +0900

    12.ユーザスキーマ構築

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
-const PORT = 6000
+const PORT = 6001
 
+app.get("/", (req, res) => {
+  res.send("Hello Express")
+})
 
 app.listen(PORT, () => {
-  console.log("サーバー起動中")
+  console.log("サーバー起動中" + PORT)
 })
