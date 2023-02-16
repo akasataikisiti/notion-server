ed735edf975fc7abe1685f6176afb8ef6a3db136
akasataikisiti <nama0you0chu@gmail.com>
2023-02-16 22:41:16 +0900

    14 mongodb接続 URLのファイルを外部保存

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const PORT = 6001
+require("dotenv").config()
 
-app.get("/", (req, res) => {
-  res.send("Hello Express")
-})
+//DB接続
+try {
+  mongoose.connect(process.env.MONGODB_URL)
+  console.log("DBと接続完了")
+} catch (error) {
+  console.log(error)
+  console.log("DB接続失敗")
+}
 
 app.listen(PORT, () => {
   console.log("サーバー起動中" + PORT)
