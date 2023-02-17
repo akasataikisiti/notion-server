b8e004383fb513fc7b665b4d33f37300c454e351
akasataikisiti <nama0you0chu@gmail.com>
2023-02-18 02:01:42 +0900

    Userコントローラを別ファイルで管理

--- src/v1/routes/auth.js
+++ src/v1/routes/auth.js
@@ -1,10 +1,9 @@
 const router = require("express").Router()
-const CryptoJS = require("crypto-js")
-const JWT = require("jsonwebtoken")
 const { body, validationResult } = require("express-validator")
 
 const User = require("../models/user")
 const validation = require("../../handlers/validation")
+const userController = require("../controllers/user")
 
 //ユーザー新規登録API
 router.post("/register",
@@ -25,27 +24,9 @@ router.post("/register",
     })
   }),
   validation.validate,
-  async (req, res) => {
-    // パスワード受け取り
-    const password = req.body.password
-
-    try {
-      //パスワードの暗号化
-      req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY)
-
-      // ユーサー新規作成
-      const user = await User.create(req.body)
-      // JWTの発行
-      const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
-        expiresIn: "24h",
-      })
-      return res.status(200).json({ user, token })
-    } catch (err) {
-      return res.status(500).json(err)
-    }
-  })
+  userController.register
+)
 
 
-//ユーザーログイン用API
-//
+
 module.exports = router
