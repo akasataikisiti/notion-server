c335a9d8be019b57cd06a8c565978e536deea9b7
akasataikisiti <nama0you0chu@gmail.com>
2023-02-20 01:16:28 +0900

    修正 corsエラー回避
    クライアントからのAPI送信を受け付けないエラーの修正

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express")
 const mongoose = require("mongoose")
 const PORT = 6001
 require("dotenv").config()
-
+const cors = require("cors")
 const app = express()
+
+app.use(cors({
+  origin: "http://localhost:3000",
+}))
 app.use(express.json())
 
 app.use("/api/v1", require("./src/v1/routes/auth"))
@@ -21,3 +25,4 @@ try {
 app.listen(PORT, () => {
   console.log("サーバー起動中" + PORT)
 })
+
