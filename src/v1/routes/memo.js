ea458b394cdba0584ba7371857b2b342ae2d6b6d
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 03:40:36 +0900

    メモ取得APIとエンドポイントを作成

--- src/v1/routes/memo.js
+++ src/v1/routes/memo.js
@@ -2,8 +2,11 @@ const router = require("express").Router()
 const memoController = require("../controllers/memo")
 const tokenHandler = require("../handlers/tokenHandler")
 
-
+// メモ作成
 router.post("/", tokenHandler.verifyToken, memoController.create)
+
+// ログインユーザが投稿したメモを全て取得
+router.get("/", tokenHandler.verifyToken, memoController.getAll)
 
 module.exports = router
 
