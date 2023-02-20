b21bf65fe4fd40624e90d202957b7e74e41ab94b
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 02:51:34 +0900

    memoAPI用ルーティング設定
    memoAPIにpostする時はユーザ情報を渡してrequestしている

---
+++ src/v1/routes/memo.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+const memoController = require("../controllers/memo")
+const tokenHandler = require("../handlers/tokenHandler")
+
+
+router.post("/", tokenHandler.verifyToken, memoController.create)
+
+module.exports = router
+
