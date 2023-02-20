419da311abacfcf35aebec8239e3026d34da79a4
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 05:02:52 +0900

    メモを一つ取得するAPIの作成
    クエリ文字列が存在したら実行されるようルーティング

--- src/v1/routes/memo.js
+++ src/v1/routes/memo.js
@@ -7,6 +7,9 @@ router.post("/", tokenHandler.verifyToken, memoController.create)
 
 // ログインユーザが投稿したメモを全て取得
 router.get("/", tokenHandler.verifyToken, memoController.getAll)
+//
+// ログインユーザが投稿したメモを一つ取得
+router.get("/:memoId", tokenHandler.verifyToken, memoController.getOne)
 
 module.exports = router
 
